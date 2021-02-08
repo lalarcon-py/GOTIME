7d0f2c428fa3fbfd714216464ee482b0554fc9cb
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2021-02-07 18:58:52 -0800

    Update copy.go

--- copy.go
+++ copy.go
@@ -2,29 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
+// CopyFile is for copying file unfromation from one file to the next.
 func CopyFile(src, dst string) (err error) {
 	sfi, err := os.Stat(src)
 	if err != nil {
 		return
 	}
-	if  !sfi.Mode().IsRegular() {
-		return fmt.Errorf("CopyFile: non-regular source file %s (%q)") sfi.Name(), sfi.Mod
+	if !sfi.Mode().IsRegular() {
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
 	}
-	dfi, err :=  os.Stat(dst)
+	dfi, err := os.Stat(dst)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			return
 		}
 	} else {
 		if !(dfi.Mode().IsRegular()) {
-			return fmt.Errorf("CopyFile: non-regular source file %s (%q)"), dfi.Name()
+			return fmt.Errorf("CopyFile: non-regular source file %s (%q)", dfi.Name(), dfi.Mode().String())
 		}
 	}
-	if os.SameFile(sfi,dfi) {
+	if os.SameFile(sfi, dfi) {
 		return
 	}
+	return
 }
